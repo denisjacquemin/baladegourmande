1edcaf8251aebf3615bb9d3e4a1609b98c749dbb
Denis Jacquemin <denisjacquemin@denis-jacquemins-macbook.local>
2009-03-03 07:39:43 +0100

    Inscriptions page improved

--- public/Inscriptions_files/Inscriptions.js
+++ public/Inscriptions_files/Inscriptions.js
@@ -1,9 +1,11 @@
-// Created by iWeb 3.0 local-build-20090226
+// Created by iWeb 3.0 local-build-20090303
 
-setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({shadow_0:new IWShadow({blurRadius:1,offset:new IWPoint(6.3640,6.3640),color:'#000000',opacity:0.440000})});registry.applyEffects();}
+function hostedOnDM()
 {return false;}
 function onPageLoad()
 {loadMozillaCSS('Inscriptions_files/InscriptionsMoz.css')
-detectBrowser();fixAllIEPNGs('Media/transparent.gif');Widget.onload();fixupAllIEPNGBGs();performPostEffectsFixups()}
+detectBrowser();fixAllIEPNGs('Media/transparent.gif');Widget.onload();fixupAllIEPNGBGs();applyEffects()}
 function onPageUnload()
 {Widget.onunload();}
