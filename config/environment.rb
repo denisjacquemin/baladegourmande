b5dfb1645733f3ac29baaae8f973171165093295
Denis Jacquemin <denis.jacquemin@gmail.com>
2009-03-22 14:19:41 +0100

    GEM_PATH set in environment.rb to find fastercsv

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,7 @@
 ENV['RAILS_ENV'] ||= 'production'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-#RAILS_GEM_VERSION = '2.2.2' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.2.2' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -72,4 +72,7 @@ Rails::Initializer.run do |config|
   # Activate observers that should always be running
   # Please note that observers generated using script/generate observer need to have an _observer suffix
   # config.active_record.observers = :cacher, :garbage_collector, :forum_observer
+
+  ENV['GEM_PATH'] = '/home/denisjac/gems/usr/lib/ruby/gems/1.8'
+
 end
