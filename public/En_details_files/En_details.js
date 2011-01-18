d29e0deadcc993c09fd0dd0560d3ee90d94d0a97
Denis Jacquemin <denis.jacquemin@gmail.com>
2011-01-18 18:48:10 +0100

    update 1 bg 2011

--- public/En_details_files/En_details.js
+++ public/En_details_files/En_details.js
@@ -1,7 +1,7 @@
-// Created by iWeb 3.0.1 local-build-20100318
+// Created by iWeb 3.0.2 local-build-20110118
 
 setTransparentGifURL('Media/transparent.gif');function applyEffects()
-{var registry=IWCreateEffectRegistry();registry.registerEffects({shadow_2:new IWShadow({blurRadius:1,offset:new IWPoint(2.9389,4.0451),color:'#000000',opacity:0.440000}),shadow_1:new IWShadow({blurRadius:1,offset:new IWPoint(6.3640,6.3640),color:'#000000',opacity:0.440000}),shadow_0:new IWShadow({blurRadius:1,offset:new IWPoint(1.4142,1.4142),color:'#000000',opacity:0.440000}),shadow_3:new IWShadow({blurRadius:1,offset:new IWPoint(1.4142,1.4142),color:'#000000',opacity:0.440000}),shadow_4:new IWShadow({blurRadius:1,offset:new IWPoint(4.9497,4.9497),color:'#000000',opacity:0.440000})});registry.applyEffects();}
+{var registry=IWCreateEffectRegistry();registry.registerEffects({shadow_4:new IWShadow({blurRadius:1,offset:new IWPoint(4.9497,4.9497),color:'#000000',opacity:0.440000}),shadow_0:new IWShadow({blurRadius:1,offset:new IWPoint(1.4142,1.4142),color:'#000000',opacity:0.440000}),shadow_3:new IWShadow({blurRadius:1,offset:new IWPoint(1.4142,1.4142),color:'#000000',opacity:0.440000}),shadow_1:new IWShadow({blurRadius:1,offset:new IWPoint(6.3640,6.3640),color:'#000000',opacity:0.440000}),shadow_2:new IWShadow({blurRadius:1,offset:new IWPoint(2.9389,4.0451),color:'#000000',opacity:0.440000})});registry.applyEffects();}
 function hostedOnDM()
 {return false;}
 function onPageLoad()
--- public/Menu_files/Menu.js
+++ public/Menu_files/Menu.js
@@ -1,4 +1,4 @@
-// Created by iWeb 3.0.1 local-build-20100318
+// Created by iWeb 3.0.2 local-build-20110118
 
 setTransparentGifURL('Media/transparent.gif');function applyEffects()
 {var registry=IWCreateEffectRegistry();registry.registerEffects({shadow_0:new IWShadow({blurRadius:10,offset:new IWPoint(7.0711,7.0711),color:'#000000',opacity:0.750000})});registry.applyEffects();}
