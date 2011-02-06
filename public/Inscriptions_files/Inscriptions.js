0df9edac35779cda9d6169561b6d672d84ae1fb3
Denis Jacquemin <denis.jacquemin@gmail.com>
2011-02-06 19:13:08 +0100

    edition photo added and pub folder removed

--- public/Inscriptions_files/Inscriptions.js
+++ public/Inscriptions_files/Inscriptions.js
@@ -1,4 +1,4 @@
-// Created by iWeb 3.0.1 local-build-20100318
+// Created by iWeb 3.0.2 local-build-20110206
 
 setTransparentGifURL('Media/transparent.gif');function applyEffects()
 {var registry=IWCreateEffectRegistry();registry.registerEffects({shadow_0:new IWShadow({blurRadius:1,offset:new IWPoint(6.3640,6.3640),color:'#000000',opacity:0.440000})});registry.applyEffects();}
@@ -6,6 +6,6 @@ function hostedOnDM()
 {return false;}
 function onPageLoad()
 {loadMozillaCSS('Inscriptions_files/InscriptionsMoz.css')
-detectBrowser();fixAllIEPNGs('Media/transparent.gif');IMpreload('Inscriptions_files','shapeimage_2','0');Widget.onload();fixupAllIEPNGBGs();applyEffects()}
+detectBrowser();fixAllIEPNGs('Media/transparent.gif');Widget.onload();fixupAllIEPNGBGs();applyEffects()}
 function onPageUnload()
 {Widget.onunload();}
--- public/Menu_files/Menu.js
+++ public/Menu_files/Menu.js
@@ -1,4 +1,4 @@
-// Created by iWeb 3.0.2 local-build-20110118
+// Created by iWeb 3.0.2 local-build-20110206
 
 setTransparentGifURL('Media/transparent.gif');function applyEffects()
 {var registry=IWCreateEffectRegistry();registry.registerEffects({shadow_0:new IWShadow({blurRadius:10,offset:new IWPoint(7.0711,7.0711),color:'#000000',opacity:0.750000})});registry.applyEffects();}
@@ -6,6 +6,6 @@ function hostedOnDM()
 {return false;}
 function onPageLoad()
 {loadMozillaCSS('Menu_files/MenuMoz.css')
-fixAllIEPNGs('Media/transparent.gif');IMpreload('Menu_files','shapeimage_2','0');Widget.onload();applyEffects()}
+fixAllIEPNGs('Media/transparent.gif');Widget.onload();applyEffects()}
 function onPageUnload()
 {Widget.onunload();}
