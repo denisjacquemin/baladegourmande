46231ab9517c6fb8acb994a7b4dc140f90bc116b
Denis Jacquemin <denis.jacquemin@gmail.com>
2011-11-06 11:37:30 +0100

    date of 2012's edition added and menu 2011 removed

--- public/Menu_files/Menu.js
+++ public/Menu_files/Menu.js
@@ -1,4 +1,4 @@
-// Created by iWeb 3.0.3 local-build-20110331
+// Created by iWeb 3.0.4 local-build-20111106
 
 setTransparentGifURL('Media/transparent.gif');function applyEffects()
 {var registry=IWCreateEffectRegistry();registry.registerEffects({shadow_0:new IWShadow({blurRadius:3,offset:new IWPoint(4.2426,4.2426),color:'#000000',opacity:0.440000})});registry.applyEffects();}
