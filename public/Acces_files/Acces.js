5c4c1b64b6e7c1b94b7774dd4068093d959f0fba
Denis Jacquemin <denis.jacquemin@gmail.com>
2011-12-10 12:13:40 +0100

    date 2012 updated and album 2011 added

--- public/Acces_files/Acces.js
+++ public/Acces_files/Acces.js
@@ -1,4 +1,4 @@
-// Created by iWeb 3.0.4 local-build-20111106
+// Created by iWeb 3.0.4 local-build-20111210
 
 setTransparentGifURL('Media/transparent.gif');function applyEffects()
 {var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWPhotoFrame([IWCreateImage('Acces_files/notebook_ul.png'),IWCreateImage('Acces_files/notebook_top.png'),IWCreateImage('Acces_files/notebook_ur.png'),IWCreateImage('Acces_files/notebook_right.png'),IWCreateImage('Acces_files/notebook_lr.png'),IWCreateImage('Acces_files/notebook_bottom.png'),IWCreateImage('Acces_files/notebook_ll.png'),IWCreateImage('Acces_files/notebook_left.png')],null,0,1.000000,3.000000,2.000000,1.000000,3.000000,18.000000,16.000000,17.000000,19.000000,76.000000,123.000000,79.000000,122.000000,null,null,null,0.400000),shadow_0:new IWShadow({blurRadius:3,offset:new IWPoint(4.2426,4.2426),color:'#000000',opacity:0.440000})});registry.applyEffects();}
