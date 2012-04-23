c044c215421b368d3c1a6fec5d6111a01a0a6722
Denis Jacquemin <denis.jacquemin@gmail.com>
2012-04-23 21:20:46 +0200

    update 2012

--- public/Menu_files/Menu.js
+++ public/Menu_files/Menu.js
@@ -1,4 +1,4 @@
-// Created by iWeb 3.0.4 local-build-20111210
+// Created by iWeb 3.0.4 local-build-20120423
 
 setTransparentGifURL('Media/transparent.gif');function applyEffects()
 {var registry=IWCreateEffectRegistry();registry.registerEffects({shadow_0:new IWShadow({blurRadius:3,offset:new IWPoint(4.2426,4.2426),color:'#000000',opacity:0.440000})});registry.applyEffects();}
@@ -6,6 +6,6 @@ function hostedOnDM()
 {return false;}
 function onPageLoad()
 {loadMozillaCSS('Menu_files/MenuMoz.css')
-fixAllIEPNGs('Media/transparent.gif');Widget.onload();applyEffects()}
+fixAllIEPNGs('Media/transparent.gif');Widget.onload();fixupAllIEPNGBGs();applyEffects()}
 function onPageUnload()
 {Widget.onunload();}
