ab6edcf24366b6cc486186bf37a72f32a243831f
Denis Jacquemin <denis.jacquemin@gmail.com>
2012-06-30 19:55:28 +0200

    adding sold out image

--- public/Inscriptions_files/Inscriptions.js
+++ public/Inscriptions_files/Inscriptions.js
@@ -1,4 +1,4 @@
-// Created by iWeb 3.0.4 local-build-20120423
+// Created by iWeb 3.0.4 local-build-20120630
 
 setTransparentGifURL('Media/transparent.gif');function applyEffects()
 {var registry=IWCreateEffectRegistry();registry.registerEffects({shadow_0:new IWShadow({blurRadius:3,offset:new IWPoint(4.2426,4.2426),color:'#000000',opacity:0.440000})});registry.applyEffects();}
